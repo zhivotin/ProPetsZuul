58abd320eca12bf90177d0f5b6fa4f0403f6bf01
dencher82 <den_zh@live.com>
2021-01-27 13:22:42 +0200

    first commit

---
+++ src/main/java/propets/zuul/ProPetsZuulServerApplication.java
@@ -0,0 +1,17 @@
+package propets.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ProPetsZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProPetsZuulServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/propets/zuul/ProPetsZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package propets.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProPetsZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
